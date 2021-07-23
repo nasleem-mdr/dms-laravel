41eeb929b909fa4636faca834f4616fc154928ca
refinaldy7 <refimadras.code@gmail.com>
2021-07-24 02:02:51 +0700

    Fix seeder for NavigationSeed

--- database/seeders/NavigationSeed.php
+++ database/seeders/NavigationSeed.php
@@ -15,7 +15,7 @@ class NavigationSeed extends Seeder
     public function run()
     {
 
-        Navigation::create([
+        $roleAndPermission = Navigation::create([
             'name' => 'Role & Permission',
             'url' => null,
             'parent_id' => null,
@@ -25,32 +25,32 @@ class NavigationSeed extends Seeder
         Navigation::create([
             'name' => 'Roles',
             'url' => 'role-and-permission/roles/',
-            'parent_id' => 1,
+            'parent_id' => $roleAndPermission->id,
             'permission_name' => 'assign permission',
         ]);
 
         Navigation::create([
             'name' => 'Permissions',
             'url' => 'role-and-permission/permissions/',
-            'parent_id' => 1,
+            'parent_id' => $roleAndPermission->id,
             'permission_name' => 'assign permission',
         ]);
 
         Navigation::create([
             'name' => 'Assign Permission',
             'url' => 'role-and-permission/assignable',
-            'parent_id' => 1,
+            'parent_id' => $roleAndPermission->id,
             'permission_name' => 'assign permission',
         ]);
 
         Navigation::create([
             'name' => 'Permission to Users',
             'url' => 'role-and-permission/assign/user',
-            'parent_id' => 1,
+            'parent_id' => $roleAndPermission->id,
             'permission_name' => 'assign permission',
         ]);
 
-        Navigation::create([
+        $navigation = Navigation::create([
             'name' => 'Navigation Settings',
             'url' => null,
             'parent_id' => null,
@@ -60,18 +60,18 @@ class NavigationSeed extends Seeder
         Navigation::create([
             'name' => 'Create new menu',
             'url' => 'navigation/create',
-            'parent_id' => 2,
+            'parent_id' => $navigation->id,
             'permission_name' => 'create navigation',
         ]);
 
         Navigation::create([
             'name' => 'Navigation Table',
             'url' => 'navigation/table',
-            'parent_id' => 2,
+            'parent_id' => $navigation->id,
             'permission_name' => 'create navigation',
         ]);
 
-        Navigation::create([
+        $agency = Navigation::create([
             'name' => 'Agency',
             'url' => null,
             'parent_id' => null,
@@ -81,14 +81,14 @@ class NavigationSeed extends Seeder
         Navigation::create([
             'name' => 'Create new Agency',
             'url' => 'agency/create',
-            'parent_id' => 3,
+            'parent_id' => $agency->id,
             'permission_name' => 'create agency',
         ]);
 
         Navigation::create([
             'name' => 'Agency Table',
             'url' => 'agency/table',
-            'parent_id' => 3,
+            'parent_id' => $agency->id,
             'permission_name' => 'create agency',
         ]);
     }
