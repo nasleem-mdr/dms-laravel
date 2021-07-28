f632cb572bac21c7e578c01b3c10501596eb1da9
refinaldy7 <refimadras.code@gmail.com>
2021-07-28 13:41:04 +0700

    divide chart on dashboard base on user role

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -26,14 +26,13 @@
       </div>
     </div>
   </div>
-
+  @if (auth()->user()->hasRole('super admin'))
   <div class="col-xl-4 col-md-6">
     <div class="card text__black bg-white  mb-4">
       <div class="card-body bg- card__body-title h6">Jumlah Instansi</div>
       <div class="card-body pt-0 h3 " id="total_agencies">
         0
       </div>
-
       <div class="card-footer d-flex align-items-center justify-content-between">
         <a class=" text__blue-color stretched-link h6" href="{{ route('agency.table')}}">Lihat
           Detail</a>
@@ -83,10 +82,25 @@
     </div>
   </div>
 </div>
+@endif
 
 @endsection
 
 @push('scripts')
+
+@if (auth()->user()->hasRole('super admin'))
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-<script src="{{ asset('/js/dashboard/charts.js') }}"></script>
+<script src="{{ asset('/js/dashboard/super_admin/charts.js') }}"></script>
+@endif
+
+@if (auth()->user()->hasRole('admin'))
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('/js/dashboard/admin/charts.js') }}"></script>
+@endif
+
+@if (auth()->user()->hasRole('pegawai'))
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('/js/dashboard/employee/charts.js') }}"></script>
+@endif
+
 @endpush
