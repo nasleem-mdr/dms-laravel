a55a80e2b2ec6bd3c1e4990d4482a881c2a6c663
refinaldy7 <refimadras.code@gmail.com>
2021-08-26 14:46:50 +0700

    add activity when user logged in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\Activity;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -39,6 +41,18 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->username = $this->findUsername();
+    }
+
+    public function authenticated()
+    {
+        $user = Auth::user();
+        $user->addActivity('masuk ke sistem');
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->addActivity('keluar dari sistem');
     }
 
     public function findUsername()
